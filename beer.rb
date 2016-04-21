6ad5939a466261dae641695fa6c124f268e03c2c
Liam Fallen <leaderliam@gmail.com>
2016-04-21 18:17:43 +0200

    Learn Ruby The Hard Way Exercises Complete

---
+++ beer.rb
@@ -0,0 +1,10 @@
+def beer_and_wine(beer_count, bottles_of_wine)
+  puts "We have #{beer_count} bottles of beer left."
+  puts "We have #{bottles_of_wine} bottles of wine left."
+end
+
+puts "How much wine and beer do we have left? "
+beer_count = 10
+bottles_of_wine = 5
+
+beer_and_wine(beer_count, bottles_of_wine)
